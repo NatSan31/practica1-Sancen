9b5d67c65e4c5023fec71c60238228eae4e62825
NatSan31 <21031041@itcelaya.edu.mx>
2026-01-30 13:39:02 -0600

    nine

--- film/create.php
+++ film/create.php
@@ -1,4 +1,9 @@
 <?php
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require "../config/db.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
